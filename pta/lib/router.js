3de96c65abf07173018ac8dc4b75a0f70a1f5bc1
Melissa414 <Missy41491@gmail.com>
2017-02-22 18:52:56 -0500

    file structure & navbar/footer set

---
+++ pta/lib/router.js
@@ -0,0 +1,9 @@
+Router.configure({
+  layoutTemplate: 'masterLayout',
+  loadingTemplate: 'loading',
+  notFoundTemplate: 'notFound',
+  yieldTemplates: {
+    navbar: {to: 'navbar'},
+    footer: {to: 'footer'},
+  }
+});
